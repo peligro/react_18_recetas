f66297d7d4fbb298c46a1028fd8af9b4fad0bb05
cesar_cz <yo@cesarcancino.com>
2024-09-07 09:50:55 -0400

    se limpia rama anterior MVP

--- README.md
+++
@@ -1,2 +0,0 @@
-# react_18_recetas
-Frontend recetas para curso fullstack nestjs con prisma y react
--- src/servicios/HomeService.jsx
+++
@@ -1,6 +0,0 @@
-export async function getDatosHome()
-{
-    let respuesta = await fetch(`${import.meta.env.VITE_API_URL}recetas-helper`, {headers: {'content-type':'application/json'}});
-    const resultado = await respuesta.json();
-    return resultado;
-}
